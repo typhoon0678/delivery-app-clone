4254cbd99e6b29a8bd22ac998ab5d8b36424fa29
typhoon0678 <kth0432@naver.com>
2023-07-30 16:28:40 +0900

    codepush setting

--- App.tsx
+++ App.tsx
@@ -2,7 +2,17 @@ import * as React from 'react';
 import { Provider, useSelector } from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import CodePush, { CodePushOptions } from 'react-native-code-push';
 
+const codePushOptions: CodePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.MANUAL,
+  // 언제 업데이트를 체크하고 반영할지를 정한다.
+  // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
+  // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
+  installMode: CodePush.InstallMode.IMMEDIATE,
+  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+  // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
+};
 
 function App() {
   return (
@@ -12,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default CodePush(codePushOptions)(App);
